26df666e23be4c90dc339706cc2b4e9702bf0969
Jakub Dundalek <dundalek@gmail.com>
2023-04-29 18:43:33 +0200

    Add formatting in CI

--- shell.nix
+++ shell.nix
@@ -2,10 +2,12 @@ with import <nixpkgs> { };
 stdenv.mkDerivation {
   name = "lazy-lsp-env";
   buildInputs = [
+    babashka
     git
     luajit
     luajitPackages.serpent
     neovim
+    stylua
     watchexec # for running tests in watch mode
   ];
 }
