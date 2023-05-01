eac76ff16f4eedf82e396d4231a2d2b7358a0507
Jakub Dundalek <dundalek@gmail.com>
2023-05-01 18:51:43 +0200

    Add preferred_servers option

--- tests/helpers_spec.lua
+++ tests/helpers_spec.lua
@@ -163,3 +163,129 @@ describe("lazy-lsp", function()
     end)
   end)
 end)
+
+local fake_lspconfig = {
+  fakelsp = {
+    document_config = {
+      default_config = { filetypes = { "javascript", "python", "typescript" }, cmd = { "fakelsp-binary" } },
+    },
+  },
+  pylsp = { document_config = { default_config = { filetypes = { "python" }, cmd = { "pylsp" } } } },
+  pyright = {
+    document_config = { default_config = { filetypes = { "python" }, cmd = { "pyright-langserver", "--stdio" } } },
+  },
+  tsserver = {
+    document_config = {
+      default_config = { filetypes = { "javascript", "typescript" }, cmd = { "typescript-language-server", "--stdio" } },
+    },
+  },
+}
+
+local fake_servers = {
+  pylsp = "python39Packages.python-lsp-server",
+  pyright = "pyright",
+  fakelsp = "fakelsp-package",
+  tsserver = {
+    pkgs = {
+      "nodePackages.typescript-language-server",
+      "nodePackages.typescript",
+    },
+  },
+}
+
+local function normalize_table_values(tbl)
+  for _, v in pairs(tbl) do
+    table.sort(v)
+  end
+  return tbl
+end
+
+it("build_filetype_to_servers_index", function()
+  local filetype_to_servers = helpers.build_filetype_to_servers_index(fake_lspconfig, fake_lspconfig)
+
+  assert.same({
+    javascript = { "fakelsp", "tsserver" },
+    python = { "fakelsp", "pylsp", "pyright" },
+    typescript = { "fakelsp", "tsserver" },
+  }, normalize_table_values(filetype_to_servers))
+end)
+
+it("build_server_to_filetypes_index", function()
+  local filetype_to_servers = helpers.build_filetype_to_servers_index(fake_lspconfig, fake_lspconfig)
+  local server_to_filetypes = helpers.build_server_to_filetypes_index(filetype_to_servers)
+
+  assert.same({
+    fakelsp = { "javascript", "python", "typescript" },
+    pylsp = { "python" },
+    pyright = { "python" },
+    tsserver = { "javascript", "typescript" },
+  }, normalize_table_values(server_to_filetypes))
+end)
+
+it("build_server_to_filetypes_index", function()
+  local filetype_to_servers = helpers.build_filetype_to_servers_index(fake_lspconfig, fake_lspconfig)
+  filetype_to_servers.python = { "pyright" }
+  filetype_to_servers.typescript = { "tsserver" }
+  local server_to_filetypes = helpers.build_server_to_filetypes_index(filetype_to_servers)
+
+  assert.same({
+    fakelsp = { "javascript" },
+    pyright = { "python" },
+    tsserver = { "javascript", "typescript" },
+  }, normalize_table_values(server_to_filetypes))
+end)
+
+describe("server_configs", function()
+  it("augments commands with nix-shell", function()
+    local cfg = helpers.server_configs(fake_lspconfig, fake_servers, {})
+    assert.same({ "nix-shell", "-p", "fakelsp-package", "--run", "'fakelsp-binary'" }, cfg.fakelsp.cmd)
+    assert.same({ "nix-shell", "-p", "python39Packages.python-lsp-server", "--run", "'pylsp'" }, cfg.pylsp.cmd)
+    assert.same({ "nix-shell", "-p", "pyright", "--run", "'pyright-langserver' '--stdio'" }, cfg.pyright.cmd)
+    assert.same({
+      "nix-shell",
+      "-p",
+      "nodePackages.typescript-language-server",
+      "nodePackages.typescript",
+      "--run",
+      "'typescript-language-server' '--stdio'",
+    }, cfg.tsserver.cmd)
+  end)
+
+  it("removes excluded servers", function()
+    local cfg = helpers.server_configs(fake_lspconfig, fake_servers, {
+      excluded_servers = { "pylsp", "pyright", "tsserver" },
+    })
+    assert.same({ "nix-shell", "-p", "fakelsp-package", "--run", "'fakelsp-binary'" }, cfg.fakelsp.cmd)
+    assert.is_nil(cfg.pylsp)
+    assert.is_nil(cfg.pyright)
+    assert.is_nil(cfg.tsserver)
+  end)
+
+  it("uses default filetypes", function()
+    local cfg = helpers.server_configs(fake_lspconfig, fake_servers, {})
+    table.sort(cfg.fakelsp.filetypes)
+    table.sort(cfg.pylsp.filetypes)
+    table.sort(cfg.pyright.filetypes)
+    table.sort(cfg.tsserver.filetypes)
+    assert.same({ "javascript", "python", "typescript" }, cfg.fakelsp.filetypes)
+    assert.same({ "python" }, cfg.pylsp.filetypes)
+    assert.same({ "python" }, cfg.pyright.filetypes)
+    assert.same({ "javascript", "typescript" }, cfg.tsserver.filetypes)
+  end)
+
+  it("sets filetypes based on preferred_servers option", function()
+    local cfg = helpers.server_configs(fake_lspconfig, fake_servers, {
+      preferred_servers = {
+        python = { "pyright" },
+        typescript = "tsserver",
+      },
+    })
+    table.sort(cfg.fakelsp.filetypes)
+    table.sort(cfg.pyright.filetypes)
+    table.sort(cfg.tsserver.filetypes)
+    assert.is_nil(cfg.pylsp)
+    assert.same({ "javascript" }, cfg.fakelsp.filetypes)
+    assert.same({ "python" }, cfg.pyright.filetypes)
+    assert.same({ "javascript", "typescript" }, cfg.tsserver.filetypes)
+  end)
+end)
